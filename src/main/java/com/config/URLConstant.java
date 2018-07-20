4b5d62b6c2fe897878936e1e51e56579573920c6
macun.mc <macun.mc@alibaba-inc.com>
2018-07-20 19:40:49 +0800

    update

--- src/main/java/com/config/URLConstant.java
+++ src/main/java/com/config/URLConstant.java
@@ -4,10 +4,10 @@ public class URLConstant {
     /**
      * 钉钉网关gettoken地址
      */
-    public static final String URL_GET_TOKKEN = "https://pre-oapi.dingtalk.com/gettoken";
+    public static final String URL_GET_TOKKEN = "https://oapi.dingtalk.com/gettoken";
 
     /**
      *获取用户在企业内userId的接口URL
      */
-    public static final String URL_GET_USER_INFO = "https://pre-oapi.dingtalk.com/user/getuserinfo";
+    public static final String URL_GET_USER_INFO = "https://oapi.dingtalk.com/user/getuserinfo";
 }
