2fa4438e4bed13e35e168d66995e8ddcf39357e2
opendingtalk <31611459+opendingtalk@users.noreply.github.com>
2020-05-22 14:05:04 +0800

    Update IndexController.java
    update IndexController.java

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 企业内部E应用Quick-Start示例代码 实现了最简单的免密登录（免登）功能
+ * 企业内部内部-小程序Quick-Start示例代码 实现了最简单的免密登录（免登）功能
  */
 @RestController
 public class IndexController {
