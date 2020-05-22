d126361cfe89e6d78d23c331c36887c008976fc2
opendingtalk <31611459+opendingtalk@users.noreply.github.com>
2020-05-22 14:03:16 +0800

    Update Constant.java
    update Constant.java

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -5,11 +5,11 @@ package com.config;
  */
 public class Constant {
     /**
-     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
+     * 开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppKey
      */
     public static final String APP_KEY = "***";
     /**
-     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
+     * 开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppSecret
      */
     public static final String APP_SECRET="***";
 }
